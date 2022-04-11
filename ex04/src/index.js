bf630405a6a3ce0d0a0a44f1bbe63307874c4937
Emir Dedic <dedic.emir17@gmail.com>
2022-04-11 12:31:16 +0200

    ex01-ex06

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+const milili = [10, 25, 4];
+function myArr(mili){
+    "use strict";
+    milili[0] = 4;
+    milili[1] = 10;
+    milili[2] = 25;
+    return mili;
+}
+console.log(myArr(milili))
+// Using mili = [4, 10, 25] would be invalid
+
+// Only change code above this line
+module.exports = myArr;
