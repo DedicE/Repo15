7fb60c90286159e564c7231d51993dfd789b17d4
Emir Dedic <dedic.emir17@gmail.com>
2022-04-13 01:37:15 +0200

    Fixes

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,14 +1,14 @@
-// Only change code below this line
 const milili = [10, 25, 4];
-function myArr(mili){
-    "use strict";
-    milili[0] = 4;
-    milili[1] = 10;
-    milili[2] = 25;
+
+function myArr(mili) {
+    'use strict';
+
+    mili[0] = 4;
+    mili[1] = 10;
+    mili[2] = 25;
     return mili;
 }
-console.log(myArr(milili))
-// Using mili = [4, 10, 25] would be invalid
 
-// Only change code above this line
+console.log(myArr(["mi", "li", "25"]));
+console.log(myArr(milili));
 module.exports = myArr;
